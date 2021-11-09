1c934711904fc56e1e609d423226f5674b856e1e
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2021-11-09 18:22:35 +0600

    cleaned

--- t9.m
+++ t9.m
@@ -8,6 +8,7 @@ ln=length(xn); %find the length of the sequence
 xk=zeros(1,ln);
 %initilise an array of same size as that of input sequence
 ixk=zeros(1,ln); 
+
 %code block to find the DFT of the sequence 
 
 for k=0:ln-1
@@ -15,8 +16,9 @@ for k=0:ln-1
         xk(k+1)=xk(k+1)+(xn(n+1)*exp((-i)*2*pi*k*n/ln)); 
     end
 end
-%------- 
-%code block to plot the input sequence 
+
+%code block to plot the input sequence
+
 t=0:ln-1; 
 subplot(221); stem(t,xn); 
 grid 
@@ -26,8 +28,8 @@ title('Input Sequence');
 
 magnitude=abs(xk); 
 % Find the magnitudes of individual DFT points 
-%code block to plot the magnitude response 
-%----- 
+% code block to plot the magnitude response 
+
 t=0:ln-1; 
 subplot(222); 
 stem(t,magnitude); 
@@ -38,14 +40,15 @@ xlabel ('K'); title ('Magnitude Response');
 phase=angle(xk);
 % Find the phases of individual DFT points 
 
-%code block to plot the magnitude sequence 
+% code block to plot the magnitude sequence 
 t=0:ln-1; 
 subplot(223); stem(t,phase); 
 grid 
 ylabel ('Phase'); 
 xlabel ('K'); 
 title('Phase Response'); 
-%------- % Code block to find the IDFT of the sequence %---- 
+
+% Code block to find the IDFT of the sequence
 for n=0:ln-1 
     for k=0:ln-1 
         ixk(n+1)=ixk(n+1)+(xk(k+1)*exp(i*2*pi*k*n/ln)); 
@@ -53,7 +56,7 @@ for n=0:ln-1
 end 
 ixk=ixk./ln; 
 
-% %code block to plot the input sequence 
+% code block to plot the input sequence 
 
 t=0:ln-1; 
 subplot(224); 
